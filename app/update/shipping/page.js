f43ea85fe64815f2873b05a57ece2e7a8c1847a2
Jamil <jamiulislamjamil@gmail.com>
2024-05-23 09:27:37 +0600

    update

--- app/update/shipping/page.js
+++ app/update/shipping/page.js
@@ -3,12 +3,12 @@ import { getUserData } from "@/database/queries";
 import UpdateShipping from "@/components/profile/UpdateShipping";
 
 export default async function ShippingAddressPage() {
-
+    const session = await auth();
+    const userData = await getUserData(session?.user?.email);
 
     return (
         <>
-            {/*<UpdateShipping user={userData} />*/}
-            <h1>Update Shipping</h1>
+            <UpdateShipping user={userData} />
         </>
     );
 }
